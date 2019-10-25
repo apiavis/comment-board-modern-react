81f1967fbfe052f7c8da33ff61f5df50c5e80612
Alicia Piavis <aliciapiavis@aliciapiavis.localdomain>
2019-10-25 13:36:49 -0600

    add ApprovalCard component, and make CommentDetails a child of ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from 'faker';
 
 const App = function() {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="Nice blog post!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Yesterdat at 12:45PM" 
-                content="I like the subject!" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 9:30AM" 
-                content="I like your writing!" 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Yesterdat at 12:45PM" 
+                    content="I like the subject!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 9:30AM" 
+                    content="I like your writing!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 };
